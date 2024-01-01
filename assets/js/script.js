96df89f0814ef68b62777335dcdd52093fc33595
Joseph Keane <itjosephk2@users.noreply.github.com>
2024-01-01 18:58:18 +0000

    First Draft Responsive Design Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,12 @@ for (let choice of choices) {
 }
 // display rules when clicked
 document.getElementById('rules-button').onclick = function() {
+  document.getElementById('modal-bg').classList.remove('hidden');
   document.getElementById('modal').classList.remove('hidden');
   document.getElementById('rules-button').classList.remove('hover');
 }
 document.getElementById("close-modal").onclick = function() {
+  document.getElementById('modal-bg').classList.add('hidden');
   document.getElementById('modal').classList.add('hidden');
   document.getElementById('rules-button').classList.add('hover');
 }
@@ -32,7 +34,6 @@ function battleScreen(playerChoice) {
 
   // randomly generate a selection for the computer
   let randomInt = Math.floor(Math.random() * 3);
-  console.log(randomInt);
   // display computers choice on screen
   if (randomInt == 0) {
     document.getElementById('computer-choice').innerHTML = '<div class="choice rock-oval oval-one-large"><div class="inner-rock-oval oval-two-large"><div class="white-shadow-oval oval-three-large"><div class="white-oval oval-four-large"><img class="icon-rock-large" src="assets/images/icon-rock.svg" alt=""></div></div></div></div>';
@@ -74,25 +75,40 @@ function battleScreen(playerChoice) {
   }
   // display the winner
   // update score
+  document.getElementById('result-desktop').innerHTML = '';
+  document.getElementById('result-mobile').innerHTML = '';
+  document.getElementById('result-desktop').classList.remove('hidden');
+  document.getElementById('result-mobile').classList.remove('hidden');
   if (result == "win") {
-    document.getElementById('win').classList.remove('hidden');
     document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) + 1;
+    document.getElementById('result-desktop').appendChild(document.createTextNode("You win"));
+    document.getElementById('result-mobile').appendChild(document.createTextNode("You win"));
   }
   else if (result == "lose") {
-    document.getElementById('lose').classList.remove('hidden');
     document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) - 1;
+    document.getElementById('result-desktop').appendChild(document.createTextNode("You Lose"));
+    document.getElementById('result-mobile').appendChild(document.createTextNode("You Lose"));
   }
   else if (result == "draw") {
-    document.getElementById('draw').classList.remove('hidden');
+    document.getElementById('result-desktop').appendChild(document.createTextNode("Draw"));
+    document.getElementById('result-mobile').appendChild(document.createTextNode("Draw"));
   }
   // reset game on play again
-  document.getElementById('reset').onclick = function() {
+  document.getElementById('reset-desktop').onclick = function() {
     document.getElementById('player-choice').innerHTML = '<div class="choice-holder"></div>';
     document.getElementById('computer-choice').innerHTML = '<div class="choice-holder"></div>';
-    document.getElementById('draw').classList.add('hidden');
-    document.getElementById('lose').classList.add('hidden');
-    document.getElementById('win').classList.add('hidden');
+    document.getElementById('result-desktop').classList.add('hidden');
+    document.getElementById('result-mobile').classList.add('hidden');
     document.querySelector("[data-board-type='selector']").classList.remove("hidden");
     document.querySelector("[data-board-type='battle']").classList.add("hidden");
-  }
+  };
+  document.getElementById('reset-mobile').onclick = function() {
+    document.getElementById('player-choice').innerHTML = '<div class="choice-holder"></div>';
+    document.getElementById('computer-choice').innerHTML = '<div class="choice-holder"></div>';
+    document.getElementById('result-desktop').classList.add('hidden');
+    document.getElementById('result-mobile').classList.add('hidden');
+    document.querySelector("[data-board-type='selector']").classList.remove("hidden");
+    document.querySelector("[data-board-type='battle']").classList.add("hidden");
+  };
+
 }
